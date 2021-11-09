f8a74b18e56ce24e46a92a1b93da8b333f98b58b
lluisp arets <lluisparets@gmail.com>
2021-11-09 19:49:30 +0100

    .

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburguer").addEventListener("click", function(){
+	document.querySelector("#main-menu").classList.toggle("show");
+	document.querySelector("#main-menu").classList.toggle("hide");
+});
